0c9830b3fef4423658da8b038799c6ee06489a24
Daniel <d@stuhrs.dk>
2024-11-29 21:17:56 +0100

    Add test data

--- BookPricesJob.Test/IntegrationTest/TestBaseEndpoints.cs
+++ BookPricesJob.Test/IntegrationTest/TestBaseEndpoints.cs
@@ -3,6 +3,7 @@ using BookPricesJob.Test.Setup;
 using BookPricesJob.API.Model;
 using System.Text.Json;
 using System.Text;
+using System.Net.Http.Json;
 
 namespace BookPricesJob.Test.IntegrationTest;
 
@@ -12,11 +13,26 @@ public class TestBaseEndpoints(CustomWebApplicationFactory<Program> factory) : I
     private const string JobRunsBaseEndpoint = "/api/jobruns";
     private readonly CustomWebApplicationFactory<Program> _factory = factory;
 
+    private static async Task<HttpResponseMessage> PostJob(HttpClient client, CreateJobRequest job)
+    {
+        var content = new StringContent(
+            JsonSerializer.Serialize(job),
+            Encoding.UTF8,
+            "application/json");
+
+        var response = await client.PostAsync(JobsBaseEndpoint, content);
+        response.EnsureSuccessStatusCode();
+
+        return response;
+    }
+
     [Theory]
     [InlineData(JobsBaseEndpoint)]
     [InlineData(JobRunsBaseEndpoint)]
     public async Task Get_BaseEnpointsReturnCorrectStatusCodeAndContentType(string url)
     {
+        EnvironmentHelper.SetNecessaryEnvironmentVariables();
+
         var client = _factory.CreateClient();
 
         var response = await client.GetAsync(url);
@@ -28,20 +44,42 @@ public class TestBaseEndpoints(CustomWebApplicationFactory<Program> factory) : I
     [Fact]
     public async Task Post_JobsEndpointReturnsSuccessAndContentType()
     {
+        EnvironmentHelper.SetNecessaryEnvironmentVariables();
+
         var client = _factory.CreateClient();
 
-        var jobPayload = new CreateJobRequest()
-        {
-            Name = "Test Job 1",
-            Description = "Test Job 1 described here",
-            IsActive = true
-        };
-
-        var content = new StringContent(JsonSerializer.Serialize(jobPayload), Encoding.UTF8, "application/json");
-
-        var response = await client.PostAsync(JobsBaseEndpoint, content);
+        var response = await PostJob(client, TestData.CreateJobRequestOne);
 
         Assert.Equal(HttpStatusCode.Created, response.StatusCode);
         Assert.Equal("application/json; charset=utf-8", response.Content.Headers.ContentType?.ToString());
+    }
+
+    [Fact]
+    public async Task Post_JobRunsEndpointReturnsSuccessAndContentType()
+    {
+        EnvironmentHelper.SetNecessaryEnvironmentVariables();
+        var client = _factory.CreateClient();
+
+        var responseCreateJob = await PostJob(client, TestData.CreateJobRequestOne);
+        var jobDto = await responseCreateJob.Content.ReadFromJsonAsync<JobDto>();
+
+        var jobId = jobDto!.Id;
+        var jobRunPayload = new CreateJobRunRequest()
+        {
+            JobId = jobId,
+            Priority = "Normal"
+        };
+
+        var content = new StringContent(
+            JsonSerializer.Serialize(jobRunPayload),
+            Encoding.UTF8,
+            "application/json");
+
+        var responseCreateJobRun = await client.PostAsync(JobRunsBaseEndpoint, content);
+
+        Assert.Equal(HttpStatusCode.Created, responseCreateJobRun.StatusCode);
+        Assert.Equal(
+            "application/json; charset=utf-8",
+            responseCreateJobRun.Content.Headers.ContentType?.ToString());
     }
 }
---
+++ BookPricesJob.Test/Setup/TestData.cs
@@ -0,0 +1,13 @@
+using BookPricesJob.API.Model;
+
+namespace BookPricesJob.Test.Setup;
+
+public static class TestData
+{
+    public static CreateJobRequest CreateJobRequestOne => new()
+    {
+        Name = "Test Job 1",
+        Description = "Test Job 1 described here",
+        IsActive = true
+    };
+}
