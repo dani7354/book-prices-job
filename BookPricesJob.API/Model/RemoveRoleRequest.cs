1228ac40c7d4874bac03bd651e0577d72464cba1
Daniel <d@stuhrs.dk>
2024-12-04 11:03:36 +0100

    Add dataannotations

--- BookPricesJob.API/Model/RemoveRoleRequest.cs
+++ BookPricesJob.API/Model/RemoveRoleRequest.cs
@@ -1,8 +1,13 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace BookPricesJob.API.Model;
 
 
 public class RemoveRoleRequest
 {
+    [Required]
     public string UserName { get; set; } = null!;
+
+    [Required]
     public string RoleName { get; set; } = null!;
 }
