1ef33bf725db06cfd0aa2c66feda76bf978a0099
Daniel <d@stuhrs.dk>
2025-01-05 14:34:42 +0100

    Incorrect grants (pt. 2)

--- SQL/BookPricesJob_SetupAppUser.sql
+++ SQL/BookPricesJob_SetupAppUser.sql
@@ -4,6 +4,6 @@ CREATE DATABASE IF NOT EXISTS BookPricesJob;
 -- create migration user with password and grant access to database
 -- REPLACE THE VALUES: <user> and <password> !!!!!
 CREATE USER IF NOT EXISTS '<user>'@'%' IDENTIFIED BY '<password>';
-GRANT SELECT, INSERT, UPDATE, DELETE, CREATE, ALTER, DROP
+GRANT SELECT, INSERT, UPDATE, DELETE
 ON BookPricesJob.*
 TO '<user>'@'%';
