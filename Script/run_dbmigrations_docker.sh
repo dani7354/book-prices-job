4027cf595b5ee231bc129a303114cc197de33373
Daniel <d@stuhrs.dk>
2025-01-09 06:18:50 +0100

    Add deploy steps to workflow

--- Script/run_dbmigrations_docker.sh
+++ Script/run_dbmigrations_docker.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-[[ -z $1 ]] && echo "Usage: $0 <Docker network>" && exit 1
+[[ -n $1 ]] || { echo "Usage: $0 <Docker network>"; exit 1; }
 
 declare -r docker_image="dsp8ef9/bookprices_dbmigration:latest"
 declare -r docker_network="$1" # Network should already exist and be attachable
 
 docker pull $docker_image \
-    && docker run $docker_image --network=$docker_network
+    && docker run "$docker_image" --network="$docker_network"
+
+[[ $? -eq 0 ]] || { echo "Migration failed"; exit 1; }
