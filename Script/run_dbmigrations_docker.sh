6c3f9fe8dbdc131726ef050becb897b962a504f2
Daniel <d@stuhrs.dk>
2025-01-09 19:53:50 +0100

    Fix build

--- Script/run_dbmigrations_docker.sh
+++ Script/run_dbmigrations_docker.sh
@@ -2,10 +2,10 @@
 
 [[ -n $1 ]] || { echo "Usage: $0 <Docker network>"; exit 1; }
 
-declare -r docker_image="dsp8ef9/bookprices_dbmigration:latest"
+declare -r docker_image="dsp8ef9/bookprices_jobapi_dbmigration:latest"
 declare -r docker_network="$1" # Network should already exist and be attachable
 
 docker pull $docker_image \
-    && docker run "$docker_image" --network="$docker_network"
+    && docker run "$docker_image" --network="$docker_network" --env-file dbmigration.env
 
 [[ $? -eq 0 ]] || { echo "Migration failed"; exit 1; }
