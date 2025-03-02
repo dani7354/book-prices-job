16d072e5af9f6f9b49a03dbb519a9b7fb967c17a
Daniel <d@stuhrs.dk>
2025-03-02 07:54:30 +0100

    Add missing grant to user

--- SQL/BookPricesJob_SetupMigrationUser.sql
+++ SQL/BookPricesJob_SetupMigrationUser.sql
@@ -4,6 +4,6 @@ CREATE DATABASE IF NOT EXISTS BookPricesJob;
 -- create app user with password and grant access to database
 -- REPLACE THE VALUES: <user> and <password> !!!!!
 CREATE USER IF NOT EXISTS '<user>'@'%' IDENTIFIED BY '<password>';
-GRANT SELECT, INSERT, UPDATE, DELETE, CREATE, ALTER, DROP
+GRANT SELECT, INSERT, UPDATE, DELETE, CREATE, ALTER, DROP, REFERENCES, INDEX
 ON BookPricesJob.*
 TO '<user>'@'%';
